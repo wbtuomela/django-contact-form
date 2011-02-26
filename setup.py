febb4c78e799ca79b5938a3b5f37971c36fef259
jnns <jnns@gmx.com>
2011-02-26 18:51:04 +0100

    Version bumped to account for change to class-based generic views.

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(name='django-contact-form',
-      version='0.4a1',
+      version='0.5a1',
       description='Generic contact-form application for Django',
       author='James Bennett',
       author_email='james@b-list.org',
       url='http://code.google.com/p/django-contact-form/',
-      packages=['contact_form'],
+      packages=find_packages(),
       classifiers=['Development Status :: 4 - Beta',
                    'Environment :: Web Environment',
                    'Intended Audience :: Developers',
